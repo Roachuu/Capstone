8376973aeff4a878b0430c47bb64b4dcaa43a885
Roachuu <104908456+Roachuu@users.noreply.github.com>
2023-02-04 19:01:30 +0800

    Add files via upload

---
+++ static/js/index.js
@@ -0,0 +1,17 @@
+$(function(){
+    $("#top-nav-placeholder").load("topnav.html");
+});
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry)
+        if(entry.isIntersecting){
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
+        }
+    });
+  });
+  
+  const hiddenElements = document.querySelectorAll('.hidden');
+  hiddenElements.forEach((el) => observer.observe(el));
